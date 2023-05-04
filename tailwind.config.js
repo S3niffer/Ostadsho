7f0b040a2fccb58583afb07d4bd45d1d6d9ad080
Erphun <heydar858@gmail.com>
2023-05-05 00:37:29 +0330

    added some colors | screens | font-family | darkmode to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,38 @@ export default {
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                lightWhite: '#ffffff',
+                lightSecondaryWhite: '#ededed',
+                lightThirdWhite: '#f9f9f9',
+                lightFourthWhite: '#f5f5f5',
+                Gray: '#4b515a',
+                SecondaryGray: '#cccccc',
+                ThirdGray: '#69727d',
+                yellowColor: '#ffc737',
+                main: '#52ac66',
+                darkBlack: '##101214',
+                darkSecondaryBlack: '#201c1c',
+                darkThirdBlack: '#1a1d1f',
+                darkFourthBlack: '#2b3034'
+            }
+        }
+        ,
+        screens: {
+            'vsm': '450px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px'
+        },
+        fontFamily: {
+            IRANSans: 'IRANSans'
+        }
+    },
     theme: {
         extend: {},
     },
