e12b8a45b1da7a9e88d0725c7be6bea797a6fc72
Erphun <heydar858@gmail.com>
2023-05-06 21:41:14 +0330

    fixed some issue

--- src/Layouts/Navbar/Navbar.tsx
+++ src/Layouts/Navbar/Navbar.tsx
@@ -9,12 +9,13 @@ const NavbarItems: TnavbarItem[] = [
         ]
     },
     {
-        id: 0, title: 'روزنامه نویسی', link: '', submenu: [
+        id: 1, title: 'روزنامه نویسی', link: '', submenu: [
             { id: 0, link: '', title: 'هوش مصنوعی' },
             { id: 1, link: '', title: 'طراحی سایت' },
             { id: 2, link: '', title: 'برنامه نویسی موبایل' }
         ]
-    }
+    },
+
 ]
 
 
@@ -22,7 +23,7 @@ const Navbar = () => {
     return (
         <ul className="flex gap-5 font text-base text-Gray dark:text-lightWhite items-center pt-2">
             {NavbarItems.map(item => (
-                <NavbarItem {...item} />
+                <NavbarItem key={item.id} {...item} />
             ))}
         </ul>
     );
--- src/components/NavbarItem/NavbarItem.tsx
+++ src/components/NavbarItem/NavbarItem.tsx
@@ -8,7 +8,7 @@ export type TnavbarItem = Tnavbar & { submenu?: TSubmenuItem }
 
 const NavbarItem = (item: TnavbarItem) => {
     return (
-        <li key={item.id} className="group relative cursor-pointer p-3 rounded-xl duration-300 hover:bg-light_green hover:text-main">
+        <li className="group relative cursor-pointer p-3 rounded-xl duration-300 hover:bg-light_green hover:text-main">
             {item.title}
             {item.submenu ? (
                 <>
@@ -16,7 +16,7 @@ const NavbarItem = (item: TnavbarItem) => {
                     <div className="submenuBg absolute w-full invisible group-hover:visible h-5">
                         <ul className="absolute bg-lightWhite border  border-t-4 border-transparent rounded-2xl border-t-main p-3 w-[200px] -right-3 top-28 opacity-0 -z-10 duration-300 group-hover:z-0 group-hover:opacity-100 group-hover:visible group-hover:top-5 dark:bg-darkThirdBlack">
                             {item.submenu.map(item => (
-                                <li key={item.id} className="p-2 hover:bg-light_green rounded-lg text-Gray hover:text-main dark:text-lightWhite">
+                                <li key={item.title} className="p-2 hover:bg-light_green rounded-lg text-Gray hover:text-main dark:text-lightWhite">
                                     {item.title}
                                 </li>
                             ))}
