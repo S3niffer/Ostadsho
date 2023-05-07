78555d939e4861a2e9810b3ca9b0d3287e2aba67
Erphun <heydar858@gmail.com>
2023-05-07 21:37:32 +0330

    added darkmode styles

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -3,12 +3,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const SearchInput = () => {
     return (
-        <div className="p-3 bg-lightFourthWhite rounded-xl flex items-center gap-2">
+        <div className="p-3 bg-lightFourthWhite dark:bg-darkBlack rounded-xl flex items-center gap-2">
             <FontAwesomeIcon
                 icon={faSearch}
                 className="text-lg text-ThirdGray pt-1 cursor-pointer"
             />
-            <input type="text" className="bg-transparent text-Gray outline-none" placeholder="جستوجو..." />
+            <input type="text" className="bg-transparent dark:text-SecondaryGray outline-none" placeholder="جستوجو..." />
         </div>
     )
 }
