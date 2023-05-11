81259d5458b8c5b398faec46444e8459100ca1e7
Erphun <heydar858@gmail.com>
2023-05-11 16:34:25 +0330

    removed menbar.css (transferred to index.css)

--- src/Layouts/MenuBar/MenuBar.tsx
+++ src/Layouts/MenuBar/MenuBar.tsx
@@ -3,7 +3,6 @@ import logo from '../../Assets/logo-light-horizontal.png'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faClose } from "@fortawesome/free-solid-svg-icons"
 import SearchInput from "../../components/SearchInput/SearchInput"
-import './MenuBar.css'
 import { NavbarItems } from "../Navbar/Navbar"
 import MenuBarItem from "../../components/MenuBarItem/MenuBarItem"
 import ThemeChangerBtn from "../../components/ThemeChangerBtn/ThemeChangerBtn"
@@ -12,12 +11,12 @@ import ThemeChangerBtn from "../../components/ThemeChangerBtn/ThemeChangerBtn"
 const MenuBar = ({ active, activeHandler }: { active: boolean, activeHandler: Dispatch<React.SetStateAction<boolean>> }) => {
     return (
         <React.Fragment>
-            <div className={`Menubar fixed z-20 top-0 w-[300px] h-screen p-5 bg-lightWhite dark:bg-darkThirdBlack lg:hidden duration-300 ${active ? 'right-0' : '-right-full'}`}>
+            <div className={`Menubar fixed z-20 top-0 w-[300px] h-screen p-5 bg-lightWhite dark:bg-darkFourthBlack lg:hidden duration-300 ${active ? 'right-0' : '-right-full'}`}>
                 <div className="flex justify-between items-center mb-5">
                     <div className="logo  w-[140px] h-[52px] bg-no-repeat bg-cover ">
                         <img src={logo} alt="logo" className="w-full h-full duration-500 cursor-pointer hover:scale-110" />
                     </div>
-                    <div className="btn btn-green block p-2 px-3"
+                    <div className="btn btn-green h-10 aspect-square flex justify-center items-center"
                         onClick={() => {
                             activeHandler(false)
                         }}>
