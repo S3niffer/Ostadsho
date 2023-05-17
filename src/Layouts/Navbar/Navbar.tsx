8cf1e0e15ffd70050c481ca05f3b777294b1b67a
Erphun <heydar858@gmail.com>
2023-05-17 22:40:09 +0330

    added new TnavbarItem

--- src/Layouts/Navbar/Navbar.tsx
+++ src/Layouts/Navbar/Navbar.tsx
@@ -2,19 +2,30 @@ import NavbarItem, { TnavbarItem } from "../../components/NavbarItem/NavbarItem"
 
 export const NavbarItems: TnavbarItem[] = [
     {
-        id: 0, title: 'برنامه نویسی', link: '', submenu: [
-            { id: 0, link: '', title: 'هوش مصنوعی' },
-            { id: 1, link: '', title: 'طراحی سایت' },
-            { id: 2, link: '', title: 'برنامه نویسی موبایل' }
+        id: 0, title: 'دسترسی سریع', link: '', submenu: [
+            { id: 0, link: '', title: 'صفحه اصلی', },
+            { id: 1, link: '', title: 'صفحه فروشگاه' },
+            { id: 2, link: '', title: 'صفحه سبد خرید' }
         ]
     },
     {
-        id: 1, title: 'روزنامه نویسی', link: '', submenu: [
-            { id: 0, link: '', title: 'هوش مصنوعی' },
-            { id: 1, link: '', title: 'طراحی سایت' },
-            { id: 2, link: '', title: 'برنامه نویسی موبایل' }
+        id: 1, title: 'دسته بندی', link: '', ulColsThree: true, submenu: [
+            { id: 0, link: '', title: 'فرانت اند', },
+            { id: 1, link: '', title: 'بک اند' },
+            { id: 2, link: '', title: 'هوشی مصنوعی' },
+            { id: 3, link: '', title: 'موبایل' },
+            { id: 4, link: '', title: 'شبکه و امنیت' },
+            { id: 5, link: '', title: 'نرم افزار' },
         ]
     },
+    {
+        id: 2, title: 'مدرسان', link: '', submenu: [
+            { id: 0, link: '', title: 'محمحد معین مهرانی' },
+            { id: 1, link: '', title: 'ایمان مدائنی' },
+            { id: 2, link: '', title: 'جادی میرمیرانی' }
+        ]
+    },
+
 
 ]
 
