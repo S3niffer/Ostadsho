b5d3d19870153f044e2a703d8e6dc2e395a76a53
Erphun <heydar858@gmail.com>
2023-05-18 13:22:38 +0330

    added one props for set class to input

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,14 +1,14 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-const SearchInput = () => {
+const SearchInput = ({ className }: { className: string }) => {
     return (
         <div className="p-3 bg-lightFourthWhite dark:bg-darkBlack rounded-xl flex items-center gap-2">
             <FontAwesomeIcon
                 icon={faSearch}
                 className="text-lg text-ThirdGray pt-1 cursor-pointer"
             />
-            <input type="text" className="bg-transparent dark:text-SecondaryGray outline-none" placeholder="جستوجو..." />
+            <input type="text" className={`bg-transparent text-dark w-full dark:text-SecondaryGray outline-none ${className}`} placeholder="جستوجو..." />
         </div>
     )
 }
