5d2d47b4e2be9ad2fbc0479081b6a7ce93154ff4
Erphun <heydar858@gmail.com>
2023-05-20 06:10:21 +0330

    added themChangerBtn to footer

--- src/Layouts/Footer/Footer.tsx
+++ src/Layouts/Footer/Footer.tsx
@@ -1,16 +1,13 @@
-import { IconDefinition, faMapLocationDot, faPhoneFlip } from "@fortawesome/free-solid-svg-icons"
+import { faMapLocationDot, faPhoneFlip } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Logo from "../../components/Logo/Logo"
 import eNamadLogo from '../../Assets/eNamadLogos/enamad_logo.webp'
 import samandehi from '../../Assets/eNamadLogos/samandehi.png'
 import FooterUl from "../../components/FooterUl/FooterUl"
 import { NavbarItems } from "../Navbar/Navbar"
+import ThemeChangerBtn from "../../components/ThemeChangerBtn/ThemeChangerBtn"
 
-type contactInfo = {
-    id: number,
-    icon: IconDefinition,
-    info: string
-}
+
 const contactInfos: contactInfo[] = [
     { id: 0, icon: faMapLocationDot, info: "تهران - خیابان پاسداران - فرعی ۶۵" },
     { id: 1, icon: faPhoneFlip, info: "۰۲۱-۰۰۰۰۰۰۰۰ | ۰۲۱-۰۰۰۰۰۰۰" },
@@ -26,7 +23,10 @@ const Footer = ({ children: TopFooter, dynamicDepart }: { children: React.ReactN
                 TopFooter
             }
             <div className="container pt-4 dark:pt-0 pb-4 px-3 md:px-0">
-                <Logo />
+                <div className="flex justify-between items-center">
+                    <Logo />
+                    <ThemeChangerBtn />
+                </div>
                 <p className="mt-3 text-xs md:text-sm bml:text-base lg:text-lg text-justify">
                     پوسته و قالب استاد شو اولین و بروزترین پوسته آموزشی ایران که در سال 1401 توسط تیم پیشتاز وب کد نویسی به بازار گرم وردپرس ایران عرضه شد، شما میتوانید این پوسته را در سایت <a href="https://www.rtl-theme.com/ostadsho-wordpress-theme/" target="_blank" onMouseOver={(e) => { e.currentTarget.textContent = '(برو به صفحه محصول)' }} onMouseLeave={(e) => { e.currentTarget.textContent = 'راست چین' }} className="text-main font-danafaBold hover:text-main dark:hover:text-secnodryMain hover:underline-offset-4 hover:underline">راست چین</a> خریداری و استفاده نمایید.
                 </p>
@@ -75,7 +75,8 @@ const Footer = ({ children: TopFooter, dynamicDepart }: { children: React.ReactN
                 <div className="mt-6 bg-lightThirdWhite dark:bg-darkBlack dark:text-FourthGray border border-transparent border-lightSecondaryWhite rounded-md p-3 md:text-center">
                     <span className="text-xs md:text-sm bml:text-base lg:text-lg select-none text-justify">
                         تمامی حقوق مادر و معنوی این سایت برای
-                        <span className="text-main cursor-pointer transition-colors duration-200 hover:text-secnodryMain" onClick={() => { (document.querySelector('.App') as HTMLDivElement).scrollTo({ top: 0, behavior: 'smooth' }) }}>
+                        <span className="text-main cursor-pointer transition-colors duration-200 hover:text-secnodryMain" onClick={() => { (document.querySelector('.App') as HTMLDivElement).scrollTo({ top: 0, behavior: 'smooth' }) }}
+                        >
                             {" استادشو "}
                         </span>
                         محفوظ میباشد.
