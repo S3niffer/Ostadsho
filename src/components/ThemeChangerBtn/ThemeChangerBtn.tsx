9232ec466387f5d16205c470b82a79e4291625aa
Erphun <heydar858@gmail.com>
2023-05-20 06:08:14 +0330

    use Useselector and localstorage to handle theme

--- src/components/ThemeChangerBtn/ThemeChangerBtn.tsx
+++ src/components/ThemeChangerBtn/ThemeChangerBtn.tsx
@@ -1,26 +1,30 @@
-import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+import { ThemeChanger, getTheme } from "../../App/Slices/Theme";
+import { useMemo } from 'react'
 
 const ThemeChangerBtn = () => {
 
-    const [theme, setTheme] = useState<"DARK" | "LIGHT">("LIGHT");
+    const Dispatch = useDispatch()
+    const Theme = useSelector(getTheme)
 
-    useEffect(() => {
+    useMemo(() => {
+        let getThemeromLocalStorage = localStorage.getItem('theme')
 
-    }, [theme])
+        if (Theme === getThemeromLocalStorage) return
+
+        Dispatch(ThemeChanger())
+    }, [])
 
     return (
         <div
             className="btn bg-SecondaryGray dark:bg-main duration-500 w-16 h-12 py-[6px] px-[14px]"
             onClick={() => {
-                setTheme((prv) => {
-                    if (prv === "DARK") return "LIGHT";
-                    return "DARK";
-                });
+                Dispatch(ThemeChanger())
             }}
         >
             <span
                 className={`bg-white block w-[24px] h-full duration-500 rounded-md 
-                ${theme === "DARK" ? "translate-x-2" : "-translate-x-5"}`}
+                ${Theme === "DARK" ? "translate-x-2" : "-translate-x-5"}`}
             ></span>
         </div>
     );
