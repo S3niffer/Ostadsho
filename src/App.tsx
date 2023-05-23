498c7beb1dc7c67d605c9106dce1d77d31161b2f
Erphun <heydar858@gmail.com>
2023-05-24 02:57:40 +0330

    changed overflow-scroll to auto

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Router from "./Router";
 function App() {
 
     return (
-        <div className="App max-h-screen overflow-y-scroll font-danafa">
+        <div className="App max-h-screen overflow-y-auto font-danafa">
             <div className="content  RTL text-Gray dark:text-lightWhite bg-lightWhite dark:bg-darkBlack">
                 <Router />
             </div>
