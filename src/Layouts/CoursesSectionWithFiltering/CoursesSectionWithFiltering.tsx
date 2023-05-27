71fc2ab216ece5bb0485600f03de1ed5189c5706
Erphun <heydar858@gmail.com>
2023-05-27 22:05:15 +0330

    completed categories list in category page

--- src/Layouts/CoursesSectionWithFiltering/CoursesSectionWithFiltering.tsx
+++ src/Layouts/CoursesSectionWithFiltering/CoursesSectionWithFiltering.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import CourseCard from "../../components/CourseCard/CourseCard"
 import PriceFilter from "../../components/PriceFilter/PriceFilter"
 import SideBarBox from "../SideBarBox/SideBarBox"
@@ -7,10 +6,12 @@ import Pagination from "../Pagination/pagination"
 import AdvertisingGIF from "../../Assets/bonrailco-1.gif"
 import { useSelector } from "react-redux"
 import { getCategories, getCourses } from "../../App/Slices/Courses"
+import { Link, useParams } from "react-router-dom"
 
 const CoursesSectionWithFiltering = () => {
     const Courses = useSelector(getCourses)
     const AllCategories = useSelector(getCategories)
+    const { Category_Name } = useParams()
 
     return (
         <div className="CoursesSectionWithFiltering container px-2 md:px-0">
@@ -18,9 +19,15 @@ const CoursesSectionWithFiltering = () => {
             <div className="courses-sideBar flex flex-col md:flex-row">
                 <div className="courses-container md:flex-1">
                     <div className="grid grid-cols-1 bml:grid-cols-2 xl:grid-cols-3 gap-y-16 gap-x-4 py-8">
-                        {Courses.map((courseBox, index) => (
-                            <CourseCard key={index} {...courseBox} />
-                        ))}
+                        {Courses.map((courseBox, index) => {
+                            if (Category_Name !== "All") {
+                                if (Category_Name !== courseBox.categoryName)
+                                    return null
+                                return <CourseCard key={index} {...courseBox} />
+                            } else {
+                                return <CourseCard key={index} {...courseBox} />
+                            }
+                        })}
                     </div>
                     <Pagination />
                 </div>
@@ -30,25 +37,36 @@ const CoursesSectionWithFiltering = () => {
                     </SideBarBox>
                     <SideBarBox title="دسته بندی محصولات">
                         <ul className=" flex flex-col">
-                            {AllCategories.map((category, index) => (
-                                <li
-                                    key={category.id}
-                                    className={
-                                        AllCategories.length === ++index
-                                            ? "group mb-3 cursor-pointer"
-                                            : "border-b border-b-lightFourthWhite dark:border-b-darkThirdBlack cursor-pointer group h-9"
-                                    }
-                                >
-                                    <div className="px-2 flex justify-between items-center group-hover:border-r-[6px] transition-all duration-500 ease-out border-r-main text-lightWhite h-9 leading-9">
-                                        <p className="text-FifthGray dark:text-SecondaryGray text-xs lg:text-sm my-auto">
-                                            {category.title}
-                                        </p>
-                                        <span className="bg-main rounded-full w-4 aspect-square lg:w-5 text-xs flex justify-center items-center pt-[3px] px-[10px] select-none">
-                                            {category.courses.length}
-                                        </span>
-                                    </div>
-                                </li>
-                            ))}
+                            {AllCategories.map((category, index) => {
+                                if (Category_Name === category.categoryName)
+                                    return null
+                                return (
+                                    <Link
+                                        key={category.id}
+                                        to={
+                                            "/categories/" +
+                                            category.categoryName
+                                        }
+                                    >
+                                        <li
+                                            className={
+                                                AllCategories.length === ++index
+                                                    ? "group mb-3 cursor-pointer"
+                                                    : "border-b border-b-lightFourthWhite dark:border-b-darkThirdBlack cursor-pointer group h-9"
+                                            }
+                                        >
+                                            <div className="px-2 flex justify-between items-center group-hover:border-r-[6px] transition-all duration-500 ease-out border-r-main text-lightWhite h-9 leading-9">
+                                                <p className="text-FifthGray dark:text-SecondaryGray text-xs lg:text-sm my-auto">
+                                                    {category.title}
+                                                </p>
+                                                <span className="bg-main rounded-full w-4 aspect-square lg:w-5 text-xs flex justify-center items-center pt-[3px] px-[10px] select-none">
+                                                    {category.courses.length}
+                                                </span>
+                                            </div>
+                                        </li>
+                                    </Link>
+                                )
+                            })}
                         </ul>
                     </SideBarBox>
                     <SideBarBox title="تبلیغات" sticky>
