b4091a6b82f9007ca3f765ec1450a2d5d3a6fb10
Erphun <heydar858@gmail.com>
2023-06-04 07:37:47 +0330

    created accordion to show  chapters and episodes

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,51 @@
+import { faPlayCircle } from "@fortawesome/free-regular-svg-icons"
+import { faAngleDown, faPlay } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react"
+
+const Accordion = ({ name, episodes }: T_chapter) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <div className='accordion'>
+            <div
+                className={`flex cursor-pointer flex-col overflow-hidden  rounded-lg border bg-lightWhite transition-all duration-1000 dark:border-darkThirdBlack dark:bg-darkBlack ${
+                    !isOpen ? "max-h-11 bml:max-h-[3.4rem]" : "max-h-screen border-main dark:border-main"
+                }`}
+            >
+                <div
+                    onClick={() => setIsOpen(p => !p)}
+                    className='flex items-center justify-between border-b p-3 text-sm dark:border-b-darkThirdBlack md:text-base bml:text-lg'
+                >
+                    <p>{name}</p>
+                    <FontAwesomeIcon
+                        icon={faAngleDown}
+                        className={` transition-all duration-150 ${isOpen ? "rotate-180" : "rotate-0"}`}
+                    />
+                </div>
+                <div className={`flex flex-col`}>
+                    {/*  */}
+                    {episodes.map(episode => (
+                        <div
+                            key={episode.id}
+                            className='group flex items-center justify-between border-b border-b-lightFourthWhite bg-lightWhite p-3 text-xs hover:bg-light_green dark:border-b-darkFourthBlack dark:bg-darkBlack dark:hover:bg-light_green md:text-sm'
+                        >
+                            <div className='flex items-center gap-2'>
+                                <span className='flex aspect-square w-5 items-center justify-center rounded-full border pt-0.5 dark:border-Gray xs:w-6 bml:w-8 bml:text-lg'>
+                                    {episode.id}
+                                </span>
+                                <p className='dark:text-lightThirdWhite bml:text-[16px]'>{episode.title}</p>
+                            </div>
+                            <div className='btn-green flex items-center gap-1 rounded-full px-2 text-[16px] text-xs group-hover:bg-main group-hover:text-lightWhite'>
+                                <span className='py-1'>مشاهده</span>
+                                <FontAwesomeIcon icon={faPlayCircle} />
+                            </div>
+                        </div>
+                    ))}
+                    {/*  */}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Accordion
