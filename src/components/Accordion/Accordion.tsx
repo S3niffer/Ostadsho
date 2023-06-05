2ead87422bf68a867ac7e4e809fcb719ae17884c
Erphun <heydar858@gmail.com>
2023-06-05 21:53:17 +0330

    added fade Effect shadow for users who not registered

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,5 +1,5 @@
 import { faPlayCircle } from "@fortawesome/free-regular-svg-icons"
-import { faAngleDown, faPlay } from "@fortawesome/free-solid-svg-icons"
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState } from "react"
 
@@ -7,7 +7,7 @@ const Accordion = ({ name, episodes }: T_chapter) => {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <div className='accordion'>
+        <div className=''>
             <div
                 className={`flex cursor-pointer flex-col overflow-hidden  rounded-lg border bg-lightWhite transition-all duration-1000 dark:border-darkThirdBlack dark:bg-darkBlack ${
                     !isOpen ? "max-h-11 bml:max-h-[3.4rem]" : "max-h-screen border-main dark:border-main"
@@ -23,7 +23,7 @@ const Accordion = ({ name, episodes }: T_chapter) => {
                         className={` transition-all duration-150 ${isOpen ? "rotate-180" : "rotate-0"}`}
                     />
                 </div>
-                <div className={`flex flex-col`}>
+                <div className='flex flex-col'>
                     {/*  */}
                     {episodes.map(episode => (
                         <div
@@ -31,7 +31,7 @@ const Accordion = ({ name, episodes }: T_chapter) => {
                             className='group flex items-center justify-between border-b border-b-lightFourthWhite bg-lightWhite p-3 text-xs hover:bg-light_green dark:border-b-darkFourthBlack dark:bg-darkBlack dark:hover:bg-light_green md:text-sm'
                         >
                             <div className='flex items-center gap-2'>
-                                <span className='flex aspect-square w-5 items-center justify-center rounded-full border pt-0.5 dark:border-Gray xs:w-6 bml:w-8 bml:text-lg'>
+                                <span className='flex aspect-square w-6 items-center justify-center rounded-full border pt-1 dark:border-Gray md:w-7 lg:w-8 lg:text-base'>
                                     {episode.id}
                                 </span>
                                 <p className='dark:text-lightThirdWhite bml:text-[16px]'>{episode.title}</p>
